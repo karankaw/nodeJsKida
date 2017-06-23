d5771f88b8990155186c9eb2fd5fafdcafee97a4
Karan Kaw <Karan.Kaw@lntinfotech.com>
2017-06-23 15:45:34 +0530

    Changed git ignore File and added smallTest Folder

---
+++ smallTest/ifTest.js
@@ -0,0 +1,10 @@
+var err = null; //'error event'
+if (err != null && err != undefined) {
+    console.log("You have got error")
+}
+
+if (err) {
+    console.log("You have got error")
+} else {
+    console.log("No Error!!")
+}
---
+++ smallTest/processargv.js
@@ -0,0 +1,12 @@
+'use strict';
+
+process.argv.forEach(function(argValue, argIndex, arrItself) {
+
+    console.log('Index:' + argIndex + ', Value = ' + argValue)
+        //0  Node.exe
+        //1  file.js
+        //node file.js
+})
+
+
+
