8eb4d7b8adfc5a83e25f5c28cde62879a36375f1
Karan Kaw <karankaw@hotmail.com>
2017-06-24 20:36:18 +0530

    Changes If Test code , Added Number check in standalone if

--- smallTest/ifTest.js
+++ smallTest/ifTest.js
@@ -1,10 +1,23 @@
-var err = null; //'error event'
-if (err != null && err != undefined) {
-    console.log("You have got error")
-}
+// var err = null; //'error event'
+// if (err != null && err != undefined) {
+//     console.log("You have got error")
+// }
 
-if (err) {
-    console.log("You have got error")
-} else {
-    console.log("No Error!!")
+// if (err) {
+//     console.log("You have got error")
+// } else {
+//     console.log("No Error!!")
+// }
+
+//if checks zero and non zero values helps in evaluating if say resultset has length
+//if(results.length)   results is array
+i = 8, j = -9, k = 0;
+
+if(i){
+	console.log('Length property is +ve');
+}
+if(j){
+	console.log('Length property is -ve');
+}if(k){
+    console.log('Length property is 0');
 }
